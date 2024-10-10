72f4823cbbd6d1fd0a55db65f1c67f594574edbf
Sharo <sharona.bhoelai@student.hu.nl>
2024-10-10 23:19:01 +0200

    6e poging

--- nummerraadspel.py
+++ nummerraadspel.py
@@ -5,39 +5,44 @@ blue = "\033[34m"
 
 def raadspel():
 
-    #Het getal dat geraden moet worden moet tussen de 1 en 15 zijn
+    # Het getal dat geraden moet worden moet tussen de 1 en 15 zijn
     rand_num = random.randint(1, 15)
 
-    #De gebruiker heeft 5 pogingen om het getal te raden
+    # De gebruiker heeft 5 pogingen om het getal te raden
     max_poging = 5
 
-    print(f"{blue}Hoi, welkom bij het nummerraad spel! Je mag een getal tussen de 1 en 15 raden.")
+    print(f"{blue}Hoi, welkom bij het nummerraadspel! Je mag een getal tussen de 1 en 15 raden.")
     print(f"Je hebt {max_poging} pogingen om het nummer te raden, succes!")
 
-    #Deze code geeft aan dat bij elke poging, 1 poging erbij word geteld totdat er 6 pogingen zijn gedaan
     for poging in range(max_poging):
-        gok = eval(input(f"Poging {poging + 1}: "))
-        if gok < 0 or gok > 15:
-            print("Voer alsjeblieft een getal tussen de 1 en de 15 in.")
-            #continue geeft aan dat de code verder gaat nadat er een ongeldige getal is ingevuld
-            continue
+        try:
+            # Probeer de invoer om te zetten naar een integer
+            gok = int(input(f"Raad het nummer (poging {poging + 1}): "))
 
+            # Controleer of de invoer binnen het juiste bereik valt
+            if gok < 1 or gok > 15:
+                print("Voer alsjeblieft een getal tussen de 1 en 15 in.")
+                continue  # Ga verder met de volgende poging als de invoer ongeldig is
+
+        except ValueError:
+            # Als de invoer geen integer is, geef een foutmelding
+            print(f"Ongeldige poging, geef alsjeblieft een nummer in.")
+            continue  # Ga verder met de volgende poging
+
+        # Check of de gok te hoog of te laag is
         if gok > rand_num:
-            print(str(gok) + " is te hoog! Probeer het opnieuw.")
+            print(f"{gok} is te hoog! Probeer het opnieuw.")
         elif gok < rand_num:
-            print(str(gok) + " is te laag! Probeer het opnieuw.")
+            print(f"{gok} is te laag! Probeer het opnieuw.")
         else:
-            #Hier staat de code van of het getal is geraden
+            # Het getal is correct geraden
             print(f"Toppie! Je hebt het getal {rand_num} geraden!")
-            #break geeft aan dat de gehele code hier stopt, omdat het getal is geraden
             break
-
     else:
-        #Hier geeft de code aan dat het getal niet is geraden en wat het getal was dat geraden moest geworden
-        print(f"Helaas, je hebt al je pogingen gebruikt en je hebt het getal niet kunnen raden.")
+        # Als de speler al zijn pogingen heeft gebruikt zonder het juiste getal te raden
+        print(f"Helaas, je hebt al je pogingen gebruikt en het juiste getal niet kunnen raden.")
         print(f"Het juiste getal was {rand_num}.")
 
-    input(f"Bedankt voor het spelen :)"
-          f"\nDruk op 'Enter' om terug naar het menu te gaan")
+    input("Bedankt voor het spelen :)\nDruk op 'Enter' om terug naar het menu te gaan.")
 
     return None
